8ba6e888ed9c354d399d7c811860ea1950b4eb51
Emmanuel Senft <esenft@wisc.edu>
2021-12-05 20:04:32 -0600

    Initial commit

---
+++ cpp_src/main.cpp
@@ -0,0 +1,97 @@
+#include "PandaIKRust.h"
+#include "ros/ros.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "std_msgs/Float64MultiArray.h"
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <iostream>
+#include <chrono>
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "panda_ik");
+    ros::NodeHandle nh("~");
+
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    auto get_required_param = [&](const std::string k, auto& v) {
+        if (!nh.getParam(k, v)) {
+            std::string msg = "Could not find required parameter: " + k;
+            ROS_ERROR(
+                msg.c_str()
+            );
+            return false;
+        }
+        return true;
+    };
+
+    std::string urdf;
+    if (!get_required_param("URDF", urdf)) return 0;
+    if (!init(urdf.c_str())) return 0;
+
+    std::array<double, 7> joint_angles = {0,0,0,-1.5,0,1.5,0};
+
+    std::chrono::system_clock::time_point last_msg_time = std::chrono::system_clock::now();
+    std::chrono::milliseconds minimum_msg_delay = std::chrono::milliseconds(0);
+
+    ros::Publisher pub = nh.advertise<std_msgs::Float64MultiArray>("output", 1);
+    ros::Publisher drone_pub = nh.advertise<geometry_msgs::PoseStamped>("drone_output", 1);
+    ros::Subscriber sub = nh.subscribe<geometry_msgs::PoseStamped>("input", 1,
+        [&](const geometry_msgs::PoseStamped::ConstPtr& msg) {
+
+
+            geometry_msgs::TransformStamped droneTransform;
+            try{
+                droneTransform = tfBuffer.lookupTransform("panda_link0", "drone",
+                                    ros::Time(0));
+            }
+            catch (tf2::TransformException &ex) {
+                ROS_WARN("%s",ex.what());
+                ros::Duration(1.0).sleep();
+                return;
+            }
+
+            auto pose = msg->pose;
+            std::string name = msg->header.frame_id;
+            std::array<double, 3> position = {pose.position.x, pose.position.y, pose.position.z};
+            std::array<double, 4> orientation = {pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w};
+
+
+            tf2::Quaternion q(droneTransform.transform.rotation.x,droneTransform.transform.rotation.y,droneTransform.transform.rotation.z,droneTransform.transform.rotation.w);
+            tf2::Matrix3x3 rot_drone = tf2::Matrix3x3(q);
+            double r, p, y;
+            rot_drone.getRPY(r, p, y, 2);
+            
+            std::array<double, 11> state = {joint_angles[0],joint_angles[1],joint_angles[2],joint_angles[3],joint_angles[4],joint_angles[5],joint_angles[6],droneTransform.transform.translation.x, droneTransform.transform.translation.y, droneTransform.transform.translation.z, y};
+
+            solve(state.data(), name.c_str(), position.data(), orientation.data());
+            
+            auto joint_msg = std_msgs::Float64MultiArray();
+            joint_msg.data.assign(state.data(), state.data() + 7);
+
+            auto now = std::chrono::system_clock::now();
+            while ((now - last_msg_time) < minimum_msg_delay) {
+                now = std::chrono::system_clock::now();
+            }
+            last_msg_time = now;
+
+            q.setEuler(state[10],0,0);
+            pub.publish(joint_msg);
+            auto drone_msg = geometry_msgs::PoseStamped();
+            drone_msg.header.frame_id = "panda_link0";
+            drone_msg.header.stamp = ros::Time(0);
+            drone_msg.pose.position.x=state[7];
+            drone_msg.pose.position.y=state[8];
+            drone_msg.pose.position.z=state[9];
+            drone_msg.pose.orientation.x=q[0];
+            drone_msg.pose.orientation.y=q[1];
+            drone_msg.pose.orientation.z=q[2];
+            drone_msg.pose.orientation.w=q[3];
+            drone_pub.publish(drone_msg);
+        }
+    );
+
+    ros::spin();
+}
---
+++ src/lib.rs
@@ -0,0 +1,219 @@
+extern crate nalgebra as na;
+use na::{Vector3, UnitQuaternion, Quaternion};
+
+use optimization_engine::constraints::{Constraint, Rectangle};
+use optimization_engine::{Optimizer, Problem, SolverError, panoc::*};
+
+use std::borrow::BorrowMut;
+use std::panic::catch_unwind;
+use std::sync::{Mutex};
+
+extern crate libc;
+use libc::c_char;
+use std::ffi::CStr;
+use std::str;
+
+use k::*;
+
+type Robot = k::Chain<f64>;
+
+struct SolverState {
+    panoc_cache : PANOCCache,
+    robot : Robot
+}
+
+static mut STATE: Option<Mutex<SolverState>> = None;
+
+fn charp_to_str<'a>(p : *mut c_char) -> &'a str {
+    let c_str : &CStr = unsafe {CStr::from_ptr(p)};
+    c_str.to_str().unwrap()
+}
+
+fn _init(urdf: &str) {
+    let robot = k::Chain::<f64>::from_urdf_file(urdf).unwrap();
+    // Create a set of joints from end joint
+    let panoc_cache = PANOCCache::new(11, 1e-6, 100000);
+
+
+    unsafe {
+        *STATE.borrow_mut() = Some(
+            Mutex::new(
+                SolverState {panoc_cache, robot}
+            )
+        );
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn init(urdf_p: *mut c_char) -> bool {
+    
+    let urdf = charp_to_str(urdf_p);
+    match catch_unwind(|| {
+        _init(urdf);
+    }) {
+        Err(err) => {
+            println!("{:?}", err);
+            false
+        },
+        _ => {
+            return true;
+        }
+    }
+}
+
+fn get_state<'a>() -> &'a mut Mutex<SolverState> {
+    unsafe { STATE.as_mut().unwrap() }
+}
+
+
+pub fn groove_loss(x_val: f64, t: f64, d: i32, c: f64, f: f64, g: i32) -> f64 {
+    -( (-(x_val - t).powi(d)) / (2.0 * c.powi(2) ) ).exp() + f * (x_val - t).powi(g)
+}
+
+fn position_cost(current_position: &Vector3<f64>, desired_position: &Vector3<f64>) -> f64 {
+    let n = (current_position - desired_position).norm();
+    n * n
+    //groove_loss(n, 0., 2, 0.1, 10.0, 2)
+}
+
+fn drone_cost(state: &[f64], trans: &Vector3<f64>) -> f64 {
+    let n = (state[7]-trans[0]-0.3).powi(2) + (state[8]-trans[1]).powi(2) + (state[9]-trans[2]).powi(2) + (state[10]-3.14).powi(2); // (state[10] - state[7].atan2(state[8])).powi(2)
+    n
+    //groove_loss(n, 0., 2, 0.1, 10.0, 2)
+}
+
+fn rotation_cost(current_rotation: &UnitQuaternion<f64>, desired_rotation: &UnitQuaternion<f64>) -> f64 {
+    let a = current_rotation.angle_to(desired_rotation);
+    a * a
+    //groove_loss(a, 0., 2, 0.1, 10.0, 2)
+}
+
+fn finite_difference(f: &dyn Fn(&[f64], &mut f64) -> Result<(), SolverError>, u: &[f64], grad: &mut [f64]) -> Result<(), SolverError> {
+    let h = 1000.0 * f64::EPSILON;
+    let mut f0 = 0.0;
+    f(u, &mut f0).unwrap();
+
+    let mut x = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0];
+    for i in 0..11 {
+        x[i] = u[i];
+    }
+
+    for i in 0..11 {
+        let mut fi = 0.0;
+        x[i] += h;
+        f(&x, &mut fi).unwrap();
+        grad[i] = (fi - f0) / h;
+        x[i] -= h;
+    }
+
+    Ok(())
+}
+
+#[no_mangle]
+pub extern "C" fn solve(q: *mut [f64;11], link_name: *mut c_char, 
+                        goal_x: *mut [f64;3], goal_q: *mut [f64;4]) {
+
+    let state = get_state().get_mut().unwrap();
+    let name = charp_to_str(link_name);
+
+    let position = unsafe{Vector3::from(std::ptr::read(goal_x))};
+    let orientation = unsafe{UnitQuaternion::from_quaternion(Quaternion::from(std::ptr::read(goal_q)))};
+
+    let robot = &mut state.robot;
+    let panoc_cache = &mut state.panoc_cache;
+
+    match robot.find(&name) {
+        None => {
+
+            for node in robot.iter() {
+                println!("{:?}", (*node.joint()).name);
+            }
+            println!("Couldn't find: {}", name);
+        },
+        Some(v) => {
+
+        }
+    };
+
+    let mut lb = [
+        //Panda Joint
+        -2.8973,
+        -1.7628,
+        -2.8973,
+        -3.0718,
+        -2.8973,
+        -0.0175,
+        -2.8973,
+        //Drone x,y,z,theta
+        -2.,
+        -2.,
+        0.,
+        -std::f64::consts::PI
+    ];
+    let mut ub = [
+        2.8973,
+        1.7628,
+        2.8973,
+        -0.0698,
+        2.8973,
+        3.7525,
+        2.8973,
+        2.,
+        2.,
+        2.,
+        std::f64::consts::PI
+    ];
+
+    let mut middle = [0.;11];
+    for i in 0..11 {
+        middle[i] = (ub[i] + lb[i]) / 2.;
+    }
+
+    for i in 0..7 {
+        lb[i] += 1e-1;
+        ub[i] -= 1e-1;
+    }
+    let bounds = Rectangle::new(Some(&lb), Some(&ub));
+
+    let mut q_state = unsafe{std::ptr::read(q).clone()};
+
+    // let solver = k::JacobianIkSolver::new(0.01, 0.01, 0.5, 100);
+    
+    // let arm = k::SerialChain::from_end(robot.find(&name).unwrap());
+    // arm.set_joint_positions(&joint_position).unwrap();
+    // solver.solve(&arm, &na::Isometry3::from_parts(na::Translation3::from(position), orientation)).unwrap();
+    // unsafe {
+    //     for i in 0..7 {
+    //         (*q)[i] = arm.joint_positions()[i]
+    //     }
+    // }
+    // return;
+
+    let cost = |u: &[f64], c: &mut f64| {
+        robot.set_joint_positions_clamped(&u[0..7]);
+        robot.update_transforms();
+        let trans = robot.find(&name).unwrap().world_transform().unwrap();
+        // *c = position_cost(&trans.translation.vector, &position)
+        //     + rotation_cost(&trans.rotation, &orientation);
+        *c = position_cost(&trans.translation.vector, &position);
+        *c += rotation_cost(&trans.rotation, &orientation);
+        *c += drone_cost(&u,&position);
+        Ok(())
+    };
+
+    let dcost = |u: &[f64], grad: &mut [f64]| {
+        finite_difference(&cost, u, grad)
+    };
+
+    let mut cur_cost: f64 = 0.0;
+    cost(&q_state, &mut cur_cost).unwrap();
+    let problem = Problem::new(&bounds, dcost, cost);
+    let mut opt = PANOCOptimizer::new(problem, panoc_cache).with_max_iter(100);
+
+    bounds.project(&mut q_state);
+    let _status = opt.solve(&mut q_state);
+    //println!("{:?}", robot.find(&name).unwrap().world_velocity().unwrap());
+    unsafe {
+        *q = q_state;
+    }
+}
